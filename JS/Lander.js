0a59f48f20816d0aab58b0985743051ff6902e4d
Yashodipundre <yashundre07@gmail.com>
2024-02-02 13:36:08 +0530

    New

--- JS/Lander.js
+++ JS/Lander.js
@@ -2,9 +2,14 @@ let ele = document.querySelector("#MenuBarRes");
 ele.addEventListener("click",(eve)=>{
     if(document.getElementById("SMB").style.width==="200px"){
         document.getElementById("SMB").style.width="0px";
+      
     }
     else{
         document.getElementById("SMB").style.width="200px";
     }
       
 })
+window.addEventListener("load",()=>{
+    document.querySelector(".logo").style.width="50px";
+})
+ 
